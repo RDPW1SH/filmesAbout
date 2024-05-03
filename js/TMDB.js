897f7da8ef111616beb1e6b4bd4d578d340a5d5e
RDPW1SH <rafael10ambrosio@gmail.com>
2024-05-03 13:00:08 +0100

    slick carousel

--- js/TMDB.js
+++ js/TMDB.js
@@ -1,14 +1,4 @@
 const API_KEY = "a1bf35fb81fc85767490536ac889539f";
 
-const options = {
-    method: 'GET', 
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMWJmMzVmYjgxZmM4NTc2NzQ5MDUzNmFjODg5NTM5ZiIsInN1YiI6IjY2MzBjOWVlMjQyZjk0MDEyYzFlN2MzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QNKuKhjXsAU_uPoBEG6yt5luOWd2Rjt7fqVqOGSQwfI'
-    }
-  };
-  
-  fetch('https://api.themoviedb.org/3/authentication', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+const BASE_URL = "https://api:themoviedb.org";
+const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&"
--- js/main.js
+++ js/main.js
@@ -1,11 +1,63 @@
 
-let carousel = document.getElementsByClassName('.carousel_container');
-$('carousel').slick({
-    infinite: true,
-    speed: 300,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    draggable: false
-    
+$(document).ready(function(){
+    // Fazer solicitação à API TMDB para obter dados dos filmes
+    $.ajax({
+        url: 'https://api.themoviedb.org/3/discover/movie',
+        type: 'GET',
+        data: {
+            api_key: 'a1bf35fb81fc85767490536ac889539f',
+            language: 'en-US',
+            sort_by: 'popularity.desc',
+            include_adult: false,
+            include_video: false,
+            page: 1,
+            page_size: 5
+        },
+        success: function(data) {
+            // Loop através dos resultados e criar slides do carousel
+            data.results.forEach(function(movie) {
+                var slide = '<div class="movie-slide"><img src="https://image.tmdb.org/t/p/w500/' + movie.poster_path + '"></div>';
+                $('.carousel_container').append(slide);
+            });
+
+            // Inicializar o Slick Carousel
+            $('.carousel_container').slick({
+                dots: true,
+                infinite: true,
+                autoplay: true,
+                draggable: false,
+                speed: 3000,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                responsive: [
+                  {
+                    breakpoint: 1024,
+                    settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 3,
+                      infinite: true,
+                      dots: true
+                    }
+                  },
+                  {
+                    breakpoint: 600,
+                    settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2
+                    }
+                  },
+                  {
+                    breakpoint: 480,
+                    settings: {
+                      slidesToShow: 1,
+                      slidesToScroll: 1
+                    }
+                  }
+                  // You can unslick at a given breakpoint now by adding:
+                  // settings: "unslick"
+                  // instead of a settings object
+                ]
+              });
+        }
+    });
 });
