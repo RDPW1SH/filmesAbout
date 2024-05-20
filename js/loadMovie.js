099b81558abdef1630e6d4e6835467cefd3019d9
RDPW1SH <rafael10ambrosio@gmail.com>
2024-05-20 09:46:10 +0100

    17/05/2024
    html e css da página movies.html

--- js/loadMovie.js
+++ js/loadMovie.js
@@ -1,10 +1,13 @@
 // Função para obter o parâmetro da URL
-function getUrlParameter(name) {
+function getUrlParameter(name, callback) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    var movieId = results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+    callback(movieId);
 }
 
-// Obter o ID do filme da URL
-var movieId = getUrlParameter('id');
+
+getUrlParameter('id', function(movieId) {
+    loadMovie(movieId);
+});
