1756147f38755e003eaa3c561812fbd5f371ac39
RDPW1SH <rafael10ambrosio@gmail.com>
2024-05-21 11:18:46 +0100

    load movie data | merge

--- js/loadMovie.js
+++ js/loadMovie.js
@@ -1,5 +1,6 @@
 // Função para obter o parâmetro da URL
 function getUrlParameter(name, callback) {
+
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
     var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
     var results = regex.exec(location.search);
@@ -7,7 +8,43 @@ function getUrlParameter(name, callback) {
     callback(movieId);
 }
 
-
+// Chama a função getUrlParameter passando o nome do parâmetro e a função de callback
 getUrlParameter('id', function(movieId) {
+    // Chama a função loadMovie com o ID do filme
     loadMovie(movieId);
 });
+
+function loadMovie(movieId) {
+    $.ajax({
+        url: `https://api.themoviedb.org/3/movie/${movieId}`,
+        type: 'GET',
+        data: {
+            api_key: 'a1bf35fb81fc85767490536ac889539f',
+            language: 'en-US',
+            include_video: true,
+        },
+        success: function(data) {
+
+            // Titulo
+            let movieBannerContainerTitle = document.getElementsByClassName('movie_banner_container_title')[0];
+            movieBannerContainerTitle.textContent = data.title;
+
+            // Votos - Classificação
+            let appendDivGenre = document.getElementsByClassName('movie_banner_container_classification')[0];
+            appendDivGenre.textContent = Math.round(data.vote_average * 10) / 10;
+            
+            // Imagem
+            let imageMovie = document.getElementsByClassName('movie_banner_container_classification')[0];
+
+            // Descrição
+            let movieBannerContainerOverview = document.getElementsByClassName('movie_banner_container_overview')[0];
+            movieBannerContainerOverview.textContent = data.overview;
+            console.log(data);
+
+        },
+        error: function(error) {
+            // Trata possíveis erros na requisição
+            console.error('Erro ao carregar o filme:', error);
+        }
+    });
+}
